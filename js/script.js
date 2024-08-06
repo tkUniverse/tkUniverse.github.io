f194c5af2228416af6355003fe9ce6446e3a99b3
JustRals <87564009+JustRals@users.noreply.github.com>
2024-08-05 21:32:52 -0300

    Added page argument to links

--- js/script.js
+++ js/script.js
@@ -40,11 +40,15 @@ function setCookie(name, value, options = {}) {
 }
 
 let update = function () {
+  const url = new URL(window.location.href);
+  url.searchParams.set('p', pagesInfo.pageNumber + 1);
+  window.history.replaceState({}, '', url.toString());
+
   document.querySelector('.image-container').classList.remove('page-error');
 
   let imgUrl = `https://tkuniverse.space/${pagesInfo.currentLanguage}/pages/${pagesInfo.pageNumber + 1}.png`;
   let sketchUrl = `https://tkuniverse.space/sketch/pages/${pagesInfo.pageNumber + 1}.png`;
-  let currentUrl = imgUrl;
+  let currentUrl = pagesInfo.isSketch ? sketchUrl : imgUrl;
 
   if (pagesInfo.pageNumber < 857) {
     sketchVerButton.disabled = true;
@@ -52,16 +56,10 @@ let update = function () {
     sketchVerButton.disabled = false;
   }
 
-  if (pagesInfo.isSketch) {
-    currentUrl = sketchUrl;
-  } else {
-    currentUrl = imgUrl;
-  }
-
   page.src = currentUrl;
   blurredPage.src = currentUrl;
-  download.href = currentUrl.slice(24);
-  download.download = `Twokinds Universe - ${pagesInfo.pageNumber + 1}${pagesInfo.currentLanguage}.png`;
+  downloadButton.href = currentUrl;
+  downloadButton.download = `Twokinds Universe - ${pagesInfo.pageNumber + 1}${pagesInfo.currentLanguage}.png`;
 
   page.onerror = function () {
     this.onerror = null;
@@ -76,12 +74,15 @@ let update = function () {
 
   setCookie('pagesInfo', JSON.stringify(pagesInfo));
   pageCounter.textContent = `${pagesInfo.pageNumber + 1}/${lastPageNumber}`;
-  pagesInfo.currentLanguage = languageSelect.value;
-  pagesInfo.pageSize = sizeSelect.value;
   changePageSize();
 };
 
 document.addEventListener('DOMContentLoaded', function () {
+  let pageParam = parseInt(getQueryParam('p'), 10) - 1;
+  if (isNaN(pageParam) || pageParam < 0) {
+    pageParam = null;
+  }
+
   let cookie = getCookie('pagesInfo');
   let userLang = navigator.language || navigator.userLanguage;
 
@@ -90,35 +91,34 @@ document.addEventListener('DOMContentLoaded', function () {
   } else {
     setCookie('pagesInfo', JSON.stringify(pagesInfo));
   }
-  if (pagesInfo.isSketch) {
-    sketchVerButton.textContent = 'Return';
-  } else {
-    sketchVerButton.textContent = 'Sketch';
+
+  if (pageParam !== null) {
+    pagesInfo.pageNumber = pageParam;
   }
+
+  sketchVerButton.textContent = pagesInfo.isSketch ? 'Return' : 'Sketch';
+
   if (pagesInfo.isFirstTime) {
     if (texts.hasOwnProperty(userLang)) {
       pagesInfo.currentLanguage = userLang;
-      changeUILanguage(userLang);
     } else {
       pagesInfo.currentLanguage = 'en';
-      changeUILanguage(pagesInfo.currentLanguage);
     }
+    changeUILanguage(pagesInfo.currentLanguage);
     languageSelect.value = pagesInfo.currentLanguage;
     pagesInfo.isFirstTime = false;
   } else {
     changeUILanguage(pagesInfo.currentLanguage);
     languageSelect.value = pagesInfo.currentLanguage;
   }
+  
   update();
 });
 
 let page = document.querySelector('.page');
 let blurredPage = document.querySelector('.blurred-page');
 let pageCounter = document.querySelector('.page-number');
-let releaseDate = document.querySelector('.release-date');
 let sketchVerButton = document.getElementById('sketch-ver-button');
-let speschlessVerButton = document.getElementById('speachless-ver-button');
-let goToButton = document.getElementById('go-to-page-button');
 let goToForm = document.getElementById('go-to-page-form');
 let languageSelect = document.getElementById('language');
 let sizeSelect = document.getElementById('page-size');
@@ -144,9 +144,9 @@ let changePage = function (currentPage, action) {
   } else if (action === 'last') {
     pagesInfo.pageNumber = lastPageNumber - 1;
   } else if (action === 'previous' && currentPage > 0) {
-    pagesInfo.pageNumber = pagesInfo.pageNumber - 1;
+    pagesInfo.pageNumber -= 1;
   } else if (action === 'next' && currentPage < lastPageNumber - 1) {
-    pagesInfo.pageNumber = pagesInfo.pageNumber + 1;
+    pagesInfo.pageNumber += 1;
   }
   update();
 };
@@ -162,81 +162,64 @@ goToForm.addEventListener('submit', function(evt) {
 });
 
 toolsButton.addEventListener('click', function () {
-  if (!isToolsShown) {
-    document.getElementById('tools-container').classList.remove('hidden');
-    isToolsShown = true;
-  } else {
-    document.getElementById('tools-container').classList.add('hidden');
-    isToolsShown = false;
-  }
+  document.getElementById('tools-container').classList.toggle('hidden');
+  isToolsShown = !isToolsShown;
 });
 
 let previewToPage = function (pageNumber) {
   pagesInfo.pageNumber = pageNumber;
   update();
   changeTab(1);
-}
+};
 
 let createPreviews = function() {
   let archive = document.getElementById('tab-2');
-  for (let i = 1; i < lastPageNumber + 1; i++) {
+  for (let i = 1; i <= lastPageNumber; i++) {
     let link = document.createElement('button');
-    link.onclick = () => previewToPage(i-1);
+    link.onclick = () => previewToPage(i - 1);
     archive.appendChild(link);
     let preview = document.createElement('img');
-    preview.classList.add('preview'+i);  
     preview.classList.add('preview');
     preview.src = `https://tkuniverse.space/${pagesInfo.currentLanguage}/pages/${i}.png`;
     link.appendChild(preview);
   }
-}
+};
 
 let changeTab = function(tabNumber) {
-  let targetPage = document.getElementById('tab-'+tabNumber);
-  let otherPages = document.querySelectorAll('.tab');
-  otherPages.forEach(function(otherPages) {
-    otherPages.classList.add('hidden');
-  });
-  targetPage.classList.remove('hidden');
-}
+  document.querySelectorAll('.tab').forEach(tab => tab.classList.add('hidden'));
+  document.getElementById('tab-' + tabNumber).classList.remove('hidden');
+};
 
 let changePageSize = function () {
-  page.className = '';
-  blurredPage.className = '';
-  
-  page.classList.add('page', 'justify-center');
-  page.classList.add(pagesInfo.pageSize);
-
-  blurredPage.classList.add('blurred-page', 'justify-center');
-  blurredPage.classList.add(pagesInfo.pageSize);
-}
+  page.className = `page justify-center ${pagesInfo.pageSize}`;
+  blurredPage.className = `blurred-page justify-center ${pagesInfo.pageSize}`;
+};
 
 sketchVerButton.addEventListener('click', function() {
-  if (pagesInfo.isSketch) {
-    pagesInfo.isSketch = false;
-    sketchVerButton.textContent = 'Sketch';
-  } else {
-    pagesInfo.isSketch = true;
-    sketchVerButton.textContent = 'Return';
-  }
-  changeUILanguage(pagesInfo.currentLanguage);
+  pagesInfo.isSketch = !pagesInfo.isSketch;
+  sketchVerButton.textContent = pagesInfo.isSketch ? 'Return' : 'Sketch';
   update();
 });
 
 function changeUILanguage(lang) {
   const textElements = document.querySelectorAll('[data-text]');
   textElements.forEach(element => {
-      const key = element.getAttribute('data-text');
-      if (texts[lang] && texts[lang][key]) {
-          element.textContent = texts[lang][key];
-      }
+    const key = element.getAttribute('data-text');
+    if (texts[lang] && texts[lang][key]) {
+      element.textContent = texts[lang][key];
+    }
   });
 
   const placeholderElements = document.querySelectorAll('[data-placeholder]');
   placeholderElements.forEach(element => {
-      const key = element.getAttribute('data-placeholder');
-      if (texts[lang] && texts[lang][key]) {
-          element.setAttribute('placeholder', texts[lang][key]);
-      }
+    const key = element.getAttribute('data-placeholder');
+    if (texts[lang] && texts[lang][key]) {
+      element.setAttribute('placeholder', texts[lang][key]);
+    }
   });
-};
+}
+
+function getQueryParam(param, url = window.location.href) {
+  const urlObj = new URL(url);
+  return urlObj.searchParams.get(param);
+}
