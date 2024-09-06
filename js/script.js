17155951e2f9d7eaa9954e6a5aac376000c32930
JustRals <87564009+JustRals@users.noreply.github.com>
2024-09-06 20:22:31 -0300

    Update fetch URL to use HTTPS in script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let lastPageNumber;
 
 async function fetchTotalPages() {
   try {
-    const response = await fetch('http://lastpage.tkuniverse.space/');
+    const response = await fetch('https://lastpage.tkuniverse.space/');
     const data = await response.json();
     lastPageNumber = Number(data.lastPage);
     update();
